e833781bfcbd484212cce0516bf61e998ee26ec7
unknown <lhimortinoen007@gmail.com>
2023-07-06 13:10:11 +0800

    TEST HOSTED BACKEND API

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../assests/css/Sidebar.css";
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import ArrowForwardIosRoundedIcon from "@material-ui/icons/ArrowForwardIosRounded";
